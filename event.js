3b86340e44b72e6a412dcc27f2e742968434f8af
Phan Dien <phandien.conthoi@gmail.com>
2020-03-25 21:49:35 +0700

    del

--- event.js
+++ event.js
@@ -1,20 +1,2 @@
-function hello() {
-    alert("Xin chào bạn");
+function eventElement() {
 }
-
-// function background_navigation_open() {
-//     var open=document.getElementById("background-navigation");
-//     open.setAttribute("class","background-navigation-open");
-//     var index=document.getElementsByClassName("icon-navigation");
-//     index[0].style.display="none";
-//     var ul=document.getElementsByClassName("nav-menu");
-//     ul[0].setAttribute("class","nav-menu-open");
-// }
-// function background_navigation_close() {
-//     var close=document.getElementById("background-navigation");
-//     close.setAttribute("class","background-navigation-close");
-//     var index=document.getElementsByClassName("icon-navigation");
-//     index[0].style.display="block";
-//     var ul=document.getElementsByClassName("nav-menu-open");
-//     ul[0].setAttribute("class","nav-menu-close");
-// }
